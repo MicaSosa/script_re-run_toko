92f9e730641ae685b4ed3e3b71ec47d5ab3911d0
MicaSosa <smicaelajanet@gmail.com>
2023-01-04 09:11:56 -0300

    scf_type_direct

--- psi4_run_sampled_re-run.py
+++ psi4_run_sampled_re-run.py
@@ -16,8 +16,8 @@ explicit_positions_filename = "explicit_atoms_$index$.xyz"
 implicit_charges_filename = "implicit_charges_$index$.dat"
 
 # (user defined: electronic)
-max_ram = "60GB"
-max_thr = 16
+max_ram = "200GB"
+max_thr = 32
 functional = "LRC-WPBE"
 basis_set = "def2-SVPD"
 nstates = 6
@@ -27,6 +27,7 @@ if not os.path.exists(out_folder_path): os.mkdir(out_folder_path)
 
 selected_indices = np.loadtxt(selected_indices_path, dtype=int)
 for index in selected_indices:
+    print(index)
     # (prepares paths)
     xyz_path = os.path.join(samples_folder, explicit_positions_filename.replace("$index$", str(index)))
     imp_dat_path = os.path.join(samples_folder, implicit_charges_filename.replace("$index$", str(index)))
@@ -34,45 +35,51 @@ for index in selected_indices:
     out_path = os.path.join(out_folder_path, f"{out_tag}_{index}")
     if not os.path.exists(out_path): os.mkdir(out_path)
     
-    f = False
 
     # (in vacuum)
+    f = False
     this_tag = "vac_" + out_tag
     out_file_path = os.path.join(out_path,f"{this_tag}_td.out")
     try: 
         f = open(out_file_path,"r")
         lines = f.readlines()
+        print(f"Si ha corrido {this_tag}_td.out\n")
     except FileNotFoundError:
         print(f"No está corrido {this_tag}_td.out\n")
         psi4_calc(xyz_path, functional, basis_set, nstates, max_ram, max_thr, optimize=False, out_tag=this_tag, output_folder=out_path)
     if f:
         for i,line in enumerate(f):
-            if "Excited State    1" in line:
+            if "Excited State    6" in line:
+                print("Y {this_tag}_td.out ha terimando completamente")
                 break
             elif i==(len(lines)-1):
+                print("No terminó todo, {this_tag}_td.out correrá ahora")
                 psi4_calc(xyz_path, functional, basis_set, nstates, max_ram, max_thr, optimize=False, out_tag=this_tag, output_folder=out_path)
-    
-    f = False
+
 
     # (with implicit charges)
+    f = False
     this_tag = "imp_" + out_tag
     out_file_path = os.path.join(out_path,f"{this_tag}_td.out")
     try: 
         f = open(out_file_path,"r")
         lines = f.readlines()
+        print(f"Si ha corrido {this_tag}_td.out\n")
     except FileNotFoundError:
         print(f"No está corrido {this_tag}_td.out\n")
         psi4_calc(xyz_path, functional, basis_set, nstates, max_ram, max_thr, optimize=False, out_tag=this_tag, external_charges=imp_dat_path, output_folder=out_path)
     if f:
         for i,line in enumerate(f):
-            if "Excited State    1" in line:
-                break
+            if "Excited State    6" in line:
+               print("Y {this_tag}_td.out ha terminado completamente")
+               break
             elif i==(len(lines)-1):
-                psi4_calc(xyz_path, functional, basis_set, nstates, max_ram, max_thr, optimize=False, out_tag=this_tag, external_charges=imp_dat_path, output_folder=out_path)
-
-    f = False
+               print("No terminó todo, {this_tag}_td.out correrá ahora")
+               psi4_calc(xyz_path, functional, basis_set, nstates, max_ram, max_thr, optimize=False, out_tag=this_tag, external_charges=imp_dat_path, output_folder=out_path)
+"""
 
     # (with equivalent field)
+    f = False
     this_tag = "eqf_" + out_tag
     out_file_path = os.path.join(out_path,f"{this_tag}_td.out")
     try: 
@@ -86,14 +93,14 @@ for index in selected_indices:
         psi4_calc(xyz_path, functional, basis_set, nstates, max_ram, max_thr, optimize=False, out_tag=this_tag, external_field=eq_field_vec, output_folder=out_path)
     if f:
         for i,line in enumerate(f):
-            if "Excited State    1" in line:
+            if "Excited State    6" in line:
                 break 
             elif i==(len(lines)-1):
                     imp_positions, imp_charges = load_charges_dat(imp_dat_path)
                     exp_positions = load_positions_xyz(xyz_path)
                     eq_field_vec = get_equivalent_mean_electric_field(imp_positions, imp_charges, exp_positions)
                     psi4_calc(xyz_path, functional, basis_set, nstates, max_ram, max_thr, optimize=False, out_tag=this_tag, external_field=eq_field_vec, output_folder=out_path)
-
+"""
 ##############################
 #   Andrés Ignacio Bertoni   #
 # (andresibertoni@gmail.com) #
