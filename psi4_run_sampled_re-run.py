c747b9c5be8a856508d88e1839d68b14b0ea0b0b
MicaSosa <smicaelajanet@gmail.com>
2023-01-12 13:14:29 -0300

    corren bien

--- psi4_run_sampled_re-run.py
+++ psi4_run_sampled_re-run.py
@@ -16,16 +16,16 @@ explicit_positions_filename = "explicit_atoms_$index$.xyz"
 implicit_charges_filename = "implicit_charges_$index$.dat"
 
 # (user defined: electronic)
-max_ram = "200GB"
-max_thr = 32
+max_ram = "60GB"
+max_thr = 16 
 functional = "LRC-WPBE"
 basis_set = "def2-SVPD"
-nstates = 6
+nstates = 10
 
 # (creates output containing folder)
 if not os.path.exists(out_folder_path): os.mkdir(out_folder_path)
-
 selected_indices = np.loadtxt(selected_indices_path, dtype=int)
+
 for index in selected_indices:
     print(index)
     # (prepares paths)
@@ -42,19 +42,17 @@ for index in selected_indices:
     out_file_path = os.path.join(out_path,f"{this_tag}_td.out")
     try: 
         f = open(out_file_path,"r")
-        lines = f.readlines()
-        print(f"Si ha corrido {this_tag}_td.out\n")
+        content = f.read()
+        print(f"Si ha corrido vacuum")
     except FileNotFoundError:
-        print(f"No está corrido {this_tag}_td.out\n")
-        psi4_calc(xyz_path, functional, basis_set, nstates, max_ram, max_thr, optimize=False, out_tag=this_tag, output_folder=out_path)
-    if f:
-        for i,line in enumerate(f):
-            if "Excited State    6" in line:
-                print("Y {this_tag}_td.out ha terimando completamente")
-                break
-            elif i==(len(lines)-1):
-                print("No terminó todo, {this_tag}_td.out correrá ahora")
-                psi4_calc(xyz_path, functional, basis_set, nstates, max_ram, max_thr, optimize=False, out_tag=this_tag, output_folder=out_path)
+        print(f"No está corrido vacuum")
+        psi4_calc(xyz_path, functional, basis_set, nstates, max_ram, max_thr, optimize=False, out_tag=this_tag, external_charges=imp_dat_path, output_folder=out_path)
+    if content.find("Excited State    6") != -1:
+        print("Y ha terminado completamente")
+        
+    else:
+        print("No terminó todo, correrá ahora")
+        psi4_calc(xyz_path, functional, basis_set, nstates, max_ram, max_thr, optimize=False, out_tag=this_tag, external_charges=imp_dat_path, output_folder=out_path)
 
 
     # (with implicit charges)
@@ -63,44 +61,45 @@ for index in selected_indices:
     out_file_path = os.path.join(out_path,f"{this_tag}_td.out")
     try: 
         f = open(out_file_path,"r")
-        lines = f.readlines()
-        print(f"Si ha corrido {this_tag}_td.out\n")
+        content = f.read()
+        print(f"Si ha corrido with implicit charges")
     except FileNotFoundError:
-        print(f"No está corrido {this_tag}_td.out\n")
+        print(f"No está corrido with implicit charges")
         psi4_calc(xyz_path, functional, basis_set, nstates, max_ram, max_thr, optimize=False, out_tag=this_tag, external_charges=imp_dat_path, output_folder=out_path)
-    if f:
-        for i,line in enumerate(f):
-            if "Excited State    6" in line:
-               print("Y {this_tag}_td.out ha terminado completamente")
-               break
-            elif i==(len(lines)-1):
-               print("No terminó todo, {this_tag}_td.out correrá ahora")
-               psi4_calc(xyz_path, functional, basis_set, nstates, max_ram, max_thr, optimize=False, out_tag=this_tag, external_charges=imp_dat_path, output_folder=out_path)
-"""
+    if content.find("Excited State    6") != -1:
+        print("Y ha terminado completamente")
+        
+    else:
+        print("No terminó todo, correrá ahora")
+        psi4_calc(xyz_path, functional, basis_set, nstates, max_ram, max_thr, optimize=False, out_tag=this_tag, external_charges=imp_dat_path, output_folder=out_path)
 
+"""
     # (with equivalent field)
     f = False
     this_tag = "eqf_" + out_tag
     out_file_path = os.path.join(out_path,f"{this_tag}_td.out")
     try: 
         f = open(out_file_path,"r")
-        lines = f.readlines()
+        content = f.read()
+        print(f"Si ha corrido with equivalent field")
     except FileNotFoundError:
-        print(f"No está corrido {this_tag}_td.out\n")
+        print(f"No está corrido with equivalent field")
+        imp_positions, imp_charges = load_charges_dat(imp_dat_path)
+        exp_positions = load_positions_xyz(xyz_path)
+        eq_field_vec = get_equivalent_mean_electric_field(imp_positions, imp_charges, exp_positions)
+        print(eq_field_vec)
+        psi4_calc(xyz_path, functional, basis_set, nstates, max_ram, max_thr, optimize=False, out_tag=this_tag, external_field=eq_field_vec, output_folder=out_path)
+    if content.find("Excited State    6") != -1:
+        print("Y ha terminado completamente")
+        break
+    else:
+        print("No terminó todo, correrá ahora")
         imp_positions, imp_charges = load_charges_dat(imp_dat_path)
         exp_positions = load_positions_xyz(xyz_path)
         eq_field_vec = get_equivalent_mean_electric_field(imp_positions, imp_charges, exp_positions)
         psi4_calc(xyz_path, functional, basis_set, nstates, max_ram, max_thr, optimize=False, out_tag=this_tag, external_field=eq_field_vec, output_folder=out_path)
-    if f:
-        for i,line in enumerate(f):
-            if "Excited State    6" in line:
-                break 
-            elif i==(len(lines)-1):
-                    imp_positions, imp_charges = load_charges_dat(imp_dat_path)
-                    exp_positions = load_positions_xyz(xyz_path)
-                    eq_field_vec = get_equivalent_mean_electric_field(imp_positions, imp_charges, exp_positions)
-                    psi4_calc(xyz_path, functional, basis_set, nstates, max_ram, max_thr, optimize=False, out_tag=this_tag, external_field=eq_field_vec, output_folder=out_path)
 """
+
 ##############################
 #   Andrés Ignacio Bertoni   #
 # (andresibertoni@gmail.com) #
